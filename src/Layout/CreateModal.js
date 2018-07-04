e04a02e4f674ab198903d575945d7bac03cfb51a
Mahesh Kanna <mahesh.it.kanna@gmail.com>
2018-07-04 11:16:34 +0000

    API integrated

--- src/Layout/CreateModal.js
+++ src/Layout/CreateModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { Button, Form, FormGroup, Label, Input, FormText, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
@@ -6,7 +7,16 @@ class CreateModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,     
+      title : "",
+      organiser: "",
+      org : "",
+      description : "",
+      teamsize : "",
+      startDate : "",
+      endDate : "",
+      mob : ""
+ 
     };
 
     this.toggle = this.toggle.bind(this);
@@ -18,7 +28,17 @@ class CreateModal extends React.Component {
     });
   }
 
+  onChange = (e) => {
+    // Because we named the inputs to match their corresponding values in state, it's
+    // super easy to update the state
+    // var someProperty = {...this.state.formFields}
+    // someProperty[e.target.name] = e.target.value;
+    // this.setState({someProperty});
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+    const { title, org,description, teamsize, organiser, startDate, endDate, mob} = this.state;
     return (
       <div>
         <Button color="primary" onClick={this.toggle}>{this.props.buttonLabel}</Button>
@@ -28,15 +48,19 @@ class CreateModal extends React.Component {
           <Form>
           <FormGroup>
           <Label for="teamName">Event name</Label>
-          <Input type="text" name="teamName" id="teamName" placeholder="Reactathon..." />
+          <Input type="text" name="title" id="title" placeholder="Reactathon..." value={title} onChange={this.onChange}/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Event Description</Label>
+          <Input type="textarea" name="description" id="description" placeholder="Description ..." value={description} onChange={this.onChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="exampleText">Event Organisers: Org</Label>
-          <Input type="textarea" name="text" id="exampleText" placeholder="VES, Global..." />
+          <Input type="textarea" name="org" id="org" placeholder="VES, Global..." value={org} onChange={this.onChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="exampleSelect">Select max no of members per team</Label>
-          <Input type="select" name="select" id="exampleSelect">
+          <Input type="select" name="select" id="exampleSelect" value={teamsize} onChange={this.onChange}>
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -46,26 +70,53 @@ class CreateModal extends React.Component {
         </FormGroup>
         <FormGroup>
           <Label for="startDate">Start Date</Label>
-          <Input type="date" name="email" id="startDate"  />
+          <Input type="date" name="startDate" id="startDate" value={startDate} onChange={this.onChange} />
           <Label for="endDate">End Date</Label>
-          <Input type="date" name="email" id="endDate" />
+          <Input type="date" name="endDate" id="endDate" value={endDate} onChange={this.onChange}/>
         </FormGroup>    
         <FormGroup>
           <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="john@example.com" />
+          <Input type="email" name="organiser" id="organiser" placeholder="john@example.com" value={organiser} onChange={this.onChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="contactNumber">Mobile number</Label>
-          <Input type="text" name="contactNumber" id="contactNumber" placeholder="+91-9999999999" />
+          <Input type="text" name="mob" id="mob" placeholder="+91-9999999999" value={mob} onChange={this.onChange}/>
         </FormGroup>      
       </Form>          </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Submit</Button>{' '}
+            <Button color="primary" onClick={this.onSubmit}>Submit</Button>{' '}
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
       </div>
     );
+  }
+
+  
+
+   onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
+    const { title, org, description, teamsize, organiser, startDate, endDate, mob} = this.state;
+    const url="http://localhost:4000";
+
+    const data = {
+      title : title,
+      org: org,
+      description: description,
+      teamsize: teamsize,
+      organiser: organiser,
+      startDate: startDate,
+      endDate: endDate,
+      mob: mob
+    }
+
+    axios.post(url+'/hackathon',  data )
+      .then((result) => {
+        //access the results here....
+        this.toggle();
+        window.location.reload();
+      });
   }
 }
 
